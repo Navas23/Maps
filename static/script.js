f5562ee15a458620585fbd31cb8fe5355efad852
Carlos Navas <carlosnavas1991@gmail.com>
2015-12-07 01:11:41 +0100

    Bug fixes

--- static/script.js
+++ static/script.js
@@ -1,22 +1,31 @@
 var map;
-var win = $('this');
+var win = $(this);
 
 function initMap() {
-  map = new google.maps.Map($('#inevioMap')[0], {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+  map = new google.maps.Map($('.inevioMap')[0], {
+    center: {lat: 40.3601317, lng: -3.381495},
+    zoom: 8,
+    mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+        position: google.maps.ControlPosition.TOP_CENTER
+    },
+    mapTypeControl: true
   });
 }
 
-/*win.on('ui-view-resize', function(){
-	console.log('resize');
-});*/
-
 win.on( 'ui-view-resize ui-view-maximize ui-view-unmaximize', function(){
 	console.log('max');
-	/*var center = map.getCenter();
+	var center = map.getCenter();
 	google.maps.event.trigger(map, "resize");
-	map.setCenter(center);*/
+	map.setCenter(center);
 });
 
-setTimeout(initMap, 150);
+
+var interval = setInterval(function(){
+
+  if( typeof google !== 'undefined' && typeof google.maps !== 'undefined' ){
+    clearInterval( interval );
+    initMap();
+  }
+
+}, 50);
